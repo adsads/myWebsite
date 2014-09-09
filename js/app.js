85bf73203c2a028dc2727729195520ac9f178bc2
Alex Lanzoni <thelanzolini@gmail.com>
2014-09-08 21:02:16 -0400

    set up scaffolding

--- js/app.js
+++ js/app.js
@@ -1,81 +1,39 @@
-var app = angular.module('mainPage', ['ngRoute', 'ui.bootstrap']);
+var app = angular.module('app', ['ngRoute', 'ui.bootstrap'])
 
-app.config(['$routeProvider',
-    function ($routeProvider) {
-        $routeProvider.
-        when('/', {
-            templateUrl: 'partials/home.html',
-            controller: 'homePageCtrl'
-        }).
-        when('/about', {
-            templateUrl: 'partials/about.html',
-            controller: 'aboutPageCtrl'
-        }).
-        when('/contact', {
-            templateUrl: 'partials/contact.html',
-            controller: 'contactPageCtrl'
-        }).
-        when('/courses', {
-            templateUrl: 'partials/courses.html',
-            controller: 'coursePageCtrl'
-        }).
-        when('/projects', {
-            templateUrl: 'partials/projects.html',
-            controller: 'projectPageCtrl'
-        }).
-        otherwise({
-            redirectTo: '/'
-        });
-}]);
+.config(['$routeProvider',
+  function ($routeProvider) {
+    $routeProvider.
+      when('/', {
+        templateUrl: 'partials/home.html',
+        controller: 'mainPageCtrl'
+      }).
+      when('/about', {
+        templateUrl: 'partials/about.html',
+        controller: 'mainPageCtrl'
+      }).
+      when('/contact', {
+        templateUrl: 'partials/contact.html',
+        controller: 'mainPageCtrl'
+      }).
+      when('/courses', {
+        templateUrl: 'partials/courses.html',
+        controller: 'mainPageCtrl'
+      }).
+      when('/projects', {
+        templateUrl: 'partials/projects.html',
+        controller: 'mainPageCtrl'
+      }).
+      otherwise({
+        redirectTo: '/'
+      })
+    ;
+}])
 
-app.controller('mainPageCtrl', ['$scope', '$location',
-    function ($scope, $location) {
-        $scope.isActive = function (viewLocation) {
-            var active = (viewLocation === $location.path());
-            return active;
-        };
+.controller('mainPageCtrl', ['$scope', '$location', function ($scope, $location) {
+  $scope.isActive = function (viewLocation) {
+    var active = (viewLocation === $location.path());
+    return active;
+  };
+}])
 
-    }]);
-
-app.controller('homePageCtrl', ['$scope',
-    function ($scope) {
-
-        $scope.words = "I am a home page";
-
-    }]);
-
-app.controller('aboutPageCtrl', ['$scope',
-    function ($scope) {
-
-
-        $scope.words = "I am an about page";
-
-
-    }]);
-
-app.controller('contactPageCtrl', ['$scope',
-    function ($scope) {
-
-
-        $scope.words = "I am a contact page";
-
-
-    }]);
-
-app.controller('coursePageCtrl', ['$scope',
-    function ($scope) {
-
-
-        $scope.words = "I am a course page";
-
-
-    }]);
-
-app.controller('projectPageCtrl', ['$scope',
-    function ($scope) {
-
-
-        $scope.words = "I am a project page";
-
-
-    }]);
+;
